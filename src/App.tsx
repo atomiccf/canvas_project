56e9029d0ad24eedd21e2bf5c7404af9bb0b304d
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2024-04-25 13:11:20 +0200

    add konva

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 
 
-import {Canvas} from './components/Canvas/Canvas.tsx'
+
 import './App.css'
+import {Canvas} from "./components/Canvas/Canvas.tsx";
 
 function App() {
-
-
   return (
     <>
      <Canvas />
--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,77 +1,87 @@
-import {MouseEventHandler, useRef, useState} from "react";
-import styles from './Canvas.module.css'
-import {drawLine} from './shapes/line.ts'
-import {drawRect} from './shapes/rectangle.ts'
+import React, { useState} from 'react';
+import  { Stage, Layer, Line, Rect, Image } from 'react-konva';
+import useImage from 'use-image';
+import Konva from "konva";
 
-import React from "react";
+
+type Point = number;
+type Line = Point[];
+type Rect = { x: number; y: number; width: number; height: number };
+
 type CanvasProps = {
-    image?: string ;
+
 }
 
-export const Canvas:React.FC<CanvasProps> = () => {
-
-    const canvas = useRef<HTMLCanvasElement>(null)
-    const [isDrawing, setIsDrawing] = useState(false)
-    const [drawingMode, setDrawingMode] = useState('')
-    const [x, setX] = useState<number>(0)
-    const [y, setY] = useState<number>(0)
+export const Canvas: React.FC = () => {
+    const [lines, setLines] = useState<Line[]>([]);
+    const [rects, setRects] = useState<Rect[]>([]);
+    const [isDrawing, setIsDrawing] = useState<boolean>(false);
+    const [mode, setMode] = useState<'line' | 'rect'>('line');
 
 
+    const [image] = useImage('../src/assets/react.svg');
 
-    const mouseDown: MouseEventHandler<HTMLCanvasElement> = (e) => {
-        const canvasOffset = canvas.current!.getBoundingClientRect();
-        const canvasX = Math.round(e.clientX - canvasOffset?.left) // Subtract the 'left' of the canvas
-        const canvasY = Math.round(e.clientY - canvasOffset?.top)
+    const handleMouseDown = (e: Konva.KonvaEventObject<MouseEvent>) => {
+        setIsDrawing(true);
+        const stage = e.target.getStage();
+        if (stage) {
+            const pos = stage.getPointerPosition();
+            if (mode === 'line' && pos) {
+                setLines([...lines, [pos.x, pos.y]]);
+            } else if (mode === 'rect' && pos) {
+                setRects([...rects, {x: pos.x, y: pos.y, width: 0, height: 0}]);
+            }
+        }
+    };
 
-        setX(canvasX)
-        setY(canvasY)
-        setIsDrawing(true)
-    }
- 
-    const mouseMove: MouseEventHandler<HTMLCanvasElement> = (e) => {
-        const context = canvas.current!.getContext('2d')
-        const canvasOffset = canvas.current!.getBoundingClientRect();
-        if (!isDrawing) return
-        const newX = Math.round(e.clientX - canvasOffset?.left) // Subtract the 'left' of the canvas
-        const newY = Math.round(e.clientY - canvasOffset?.top)
-        context!.clearRect(0, 0, canvas.current!.width, canvas.current!.height); // Clear the canvas
-        switch (drawingMode) {
-            case 'line':
-                drawLine(context, x, y, newX, newY)
-                break
-              case 'rectangle':
-               drawRect(context, x, y, newX - x, newY - y)
-              break
+    const handleMouseMove = (e: Konva.KonvaEventObject<MouseEvent>) => {
+        if (!isDrawing) return;
+        const stage = e.target .getStage();
+        if (stage) {
+            const pos = stage.getPointerPosition();
+            const point = stage.getPointerPosition();
+            const lastLine = lines[lines.length - 1];
+            const lastRect = rects[rects.length - 1];
+            if (mode === 'line' && pos) {
+                setLines(lines => {
+
+                    return [
+                        ...lines.slice(0, -1),
+                        [lastLine[0], lastLine[1], pos.x, pos.y]
+                    ];
+                });
+            } else if (mode === 'rect' && point) {
+                lastRect.width = point.x - lastRect.x;
+                lastRect.height = point.y - lastRect.y;
+                rects.splice(rects.length - 1, 1, lastRect);
+                setRects(rects.concat());
+            }
         }
 
-/*        setX(newX)
-        setY(newY)*/
-    }
 
-    const mouseUp = () => {
-        if (!isDrawing) return
-        setIsDrawing(false)
-    }
+
+
+    };
+
+    const handleMouseUp = () => {
+        setIsDrawing(false);
+    };
 
     return (
         <>
-            <div className={styles.toolbar}>
-                <button
-                    onClick={() => {
-                        setDrawingMode('line')
-                    }}>
-                    Line
-                </button>
-                <button
-          onClick={() => {
-            setDrawingMode('rectangle')
-          }}>
-          Rect
-        </button>
-            </div>
-            <canvas onMouseDown={mouseDown} onMouseMove={mouseMove} onMouseUp={mouseUp} id="canvas" width={'640px'}
-                    height={'640px'} ref={canvas!}></canvas>
+                <button onClick={() => setMode('line')}>Line</button>
+                <button onClick={() => setMode('rect')}>Rect</button>
+                <Stage width={640} height={640} onMouseDown={handleMouseDown} onMousemove={handleMouseMove}
+                       onMouseup={handleMouseUp}>
+                    <Layer>
+                        <Image image={image} />
+                    </Layer>
+                    <Layer>
+                        {lines.map((line, i) => <Line key={i} points={line} stroke="black"/>)}
+                        {rects.map((rect, i) => <Rect key={i} x={rect.x} y={rect.y} width={rect.width}
+                           height={rect.height} stroke="black"/>)}
+                    </Layer>
+                </Stage>
         </>
-
-    )
-}
+    );
+};
